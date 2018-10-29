f3749e54db2ca5aa95e94842698bfd07a1e9ba50
tchewik <elenachistov@gmail.com>
2018-10-29 20:25:06 +0300

    Refactoring

--- lab_2_tbot/src/strings.py
+++ lab_2_tbot/src/strings.py
@@ -1,7 +1,10 @@
-hi = "Hello!\n\
-I'm the bot that helps you to find three drugstores or supermarkets nearby where you can purchase some condoms. \n\
-Sent me the /help command if you are confused."
-help = "Just send me your current location, and I'll find three places where you can get them right now."
-button_text = "I solemnly swear that I'm up to no good."
-start_finding_text = "Well..."
-final_text = "Good luck!"
+class AvailableStrings:
+    button_text = "I solemnly swear that I'm up to no good."
+    error_token_not_found = "Please, run 'source SECURITY.sh' beforehand!"
+    error_token_invalid = "You entered invalid token."
+    finding_text_start = "Well..."
+    finding_text_final = "Good luck!"
+    help = "Just send me your current location, and I'll find three places where you can get them right now."
+    hi = "Hello! " + \
+         "I'm the bot that helps you to find three pharmacies or supermarkets nearby where you can purchase some condoms." + \
+         "Send me the /help command if you are confused."
